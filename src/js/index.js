ae16470c735635abfa6acf154a7de6bbe8264617
RafaelDaSilva-DEV <102332823+RafaelDaSilva-DEV@users.noreply.github.com>
2022-03-24 22:35:36 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,25 @@
+const listaSelecaoPokemons = document.querySelectorAll('.pokemon')
+const pokemonsCard = document.querySelectorAll('.cartao-pokemon')
+listaSelecaoPokemons.forEach(pokemon => {
+    //um evento d clique feito pelo usuário na listagem pokemon
+    pokemon.addEventListener('click', () => {
+
+        // remover a classe aberto só do cartão que esta em aberto
+        const cartaoPokemonAberto = document.querySelector('.aberto')
+        cartaoPokemonAberto.classList.remove ('aberto')
+        //ao clicar em um pokemon da listagem pegamos o id desse pokemon para saber qual cartão mostrar 
+         const idPokemonSelecionado = pokemon.attributes.id.value
+
+         const idDoCartaoPokemonParaAbrir = 'cartao-' + idPokemonSelecionado
+        const cartaoPokemonParaAbrir = document.getElementById(idDoCartaoPokemonParaAbrir)
+        cartaoPokemonParaAbrir.classList.add('aberto')
+
+        //remover a classe ativo que marca o pokémon selecionado
+        const pokemonAtivoNaListagem = document.querySelector('.ativo')
+        pokemonAtivoNaListagem.classList.remove('ativo')
+
+        //adicionar a classe ativo no item da lista selecionado 
+        const pokemonSelecionadoNaListagem = document.getElementById(idPokemonSelecionado)
+        pokemonSelecionadoNaListagem.classList.add('ativo')
+    })
+})
